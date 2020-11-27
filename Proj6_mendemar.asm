0504c62be93ead6c656fc3fb2911c9908b4d25f2
Mark <themarkymarkers@gmail.com>
2020-11-26 22:41:01 -0800

    WriteVal works with negatives

--- Proj6_mendemar.asm
+++ Proj6_mendemar.asm
@@ -78,7 +78,9 @@ invalidErrorMsg	BYTE	"ERROR: You did not enter a signed number or your number wa
 tryAgain		BYTE	"Please try again: ",0
 userInt			SDWORD	?																			; int value after conversion from string
 charsEntered	DWORD	?																			; how many characters the user entered
+isNegative		DWORD	?
 
+negativeSign    BYTE    45,0
 userStringOut	BYTE	MAXSIZE DUP(?)
 
 youEntered		BYTE	"You entered the following numbers: ",10,13,0
@@ -90,6 +92,7 @@ thanks			BYTE	"Thanks for playing! I had so much fun",0
 .code
 main PROC
   ; get valid number from user
+  push OFFSET isNegative
   push OFFSET userInt
   push OFFSET charsEntered
   push OFFSET prompt
@@ -100,6 +103,8 @@ main PROC
 
   ; convert DWORD integer to ASCII string and print
   call CrLf
+  push OFFSET negativeSign
+  push isNegative
   push charsEntered
   push OFFSET userStringOut
   push charsEntered
@@ -124,9 +129,11 @@ main ENDP
 ;
 ; Receives:
 ;   [ebp+8]  SDWORD = integer to print (32 bits or fewer)
-;	[ebp+12] DWORD  = number of digits in integer to print  TODO: subtract one if sign
+;	[ebp+12] DWORD  = charsEntered: number of digits in integer to print
 ;	[ebp+16] BYTE   = OFFSET userStringOut
 ;	[ebp+20] DWORD  = charsEntered
+;	[ebp+24] BYTE	= isNegative: 1 if the integer to print is a negative number; 0 if positive
+;   [ebp+28] BYTE   = OFFSET negativeSign: memory location of ascii code of negative sign
 ;
 ; ---------------------------------------------------------------------------------
 WriteVal PROC
@@ -141,34 +148,61 @@ WriteVal PROC
   push	EDX
   push	EDI
 
-  mov	ECX, [EBP+12]
-  ; TODO: if the first char is a sign, decrement ECX (will stop loop one early, which excludes first digit, since loop does RTL stosb )
+  mov	ECX, [EBP+12]           ; ECX = charsEntered (including "-" if any)
+  mov	EDI, [EBP+16]		    ; EDI = OFFSET userStringOut
+  add   EDI, charsEntered	    ; move pointer to prepare for writing backwards
+  dec   EDI					; EDI was already pointing at the first element before adding charsEntered
 
-  ; divide the param by 10. Quotient is the next thing to be divided, and remainder is the rightmost digit
-  mov	EAX, [EBP+8]		; EAX = the first number to divide
-  mov	EDI, [EBP+16]		; EDI = OFFSET userStringOut
-  add	EDI, charsEntered	; move pointer to prepare for writing backwards
-  dec	EDI					; EDI was already pointing at the first element before adding charsEntered
+  ; if a negative number, undo processing from earlier and assign 2's comp to EAX
+  mov	AL, [EBP+24]            ; AL = isNegative
+  cmp	AL, 1				    ; compare isNegative to 1 (true)
+  jne	_numberIsPositive	    ; number is positive! Move on and convert to string
 
-  _convertToString:  
-  mov	EBX, 10
-  cdq
-  idiv	EBX					; now EAX contains the next thing to divide, and EDX contains rightmost digit
+  ; number is negative
+  ; TODO: good place for a helper macro
+  ; print a negative sign
+  mov	EDX, [EBP+28]		    ; EDX = (ascii for "-")
+  call	WriteString
 
-  push	EAX					; save next number to divide
-  mov	AL, DL				; no data is lost, because each converted result is only 1 byte
-  add	AL, '0'			    ; convert to string
+  ; decrement loop counter and userStringOut pointer to account for not including the negative sign
+  dec   ECX
+  dec   EDI
 
-  ; AL contains ASCII value to be appended to the BYTE array string
-  ; store AL into EDI (from right to left using std)
-  std
-  stosb						; [EDI] = digitChar, then EDI--
+  ; convert negative number to positive by subtracting from 0
+  mov   EAX, 0      		    ; EAX = 0
+  mov   EBX, [EBP+8]            ; EBX = userInt
+  sub   EAX, EBX                ; EAX = 0 - userInt
 
-  pop	EAX					; restore next number to divide
-  loop	_convertToString
+  ; skip numberIsPositive section
+  jmp _convertToString
+
+  ; else, it's already positive, so assign directly to EAX
+  _numberIsPositive:
+    mov   EAX, [EBP+8]		    ; EAX = the first number to divide
+
+  ; divide the param by 10. Quotient is the next thing to be divided, and remainder is the rightmost digit  
+  ; EAX = the first number to divide
+  _convertToString:
+
+  _convertNext:  
+    mov   EBX, 10
+    cdq
+    idiv  EBX					; now EAX contains the next thing to divide, and EDX contains rightmost digit
+
+    push  EAX					; save next number to divide
+    mov   AL, DL				    ; no data is lost, because each converted result is only 1 byte
+    add	  AL, '0'			    ; convert to string
+
+    ; AL contains ASCII value to be appended to the BYTE array string
+    ; store AL into EDI (from right to left using std)
+    std
+    stosb						; [EDI] = digitChar, then EDI--
+
+    pop	  EAX					; restore next number to divide
+    loop  _convertNext
 
   ; print the string
-  mDisplayString [EBP+16]	; for help reversing string, see video at https://canvas.oregonstate.edu/courses/1784177/pages/exploration-1-lower-level-programming?module_item_id=20049973 
+  mDisplayString [EBP+16]	    ; for help reversing string, see video at https://canvas.oregonstate.edu/courses/1784177/pages/exploration-1-lower-level-programming?module_item_id=20049973 
 
   pop	EDI
   pop	EDX
@@ -193,12 +227,13 @@ WriteVal ENDP
 ;	[ebp+20] = OFFSET prompt
 ;	[ebp+24] = OFFSET charsEntered
 ;	[ebp+28] = OFFSET userInt
+;	[ebp+32] = OFFSET isNegative: to be assigned 1 if value is negative; 0 otherwise
 ;
 ; Returns:
 ;	userInt SDWORD = integer entered by user
 ; ---------------------------------------------------------------------------------
 ReadVal PROC
-  local isNegative:BYTE
+  local glo:BYTE
   ; local executes...
   ;  push	EBP
   ;  mov	EBP, ESP
@@ -215,35 +250,41 @@ ReadVal PROC
   mGetString [EBP+8], [EBP+12], [EBP+16], [EBP+20], [EBP+24]
   
   ; convert userString to SDWORD int
-  mov	ESI, [EBP+24]   ; loop as many times as there are chars in what user entered
-  mov	ECX, [ESI]		;   (cont)
-  mov	EBX, 0			; tracks finalInteger
-  mov	ESI, [EBP+8]	; ESI = OFFSET userString
-  cld					; starting from the msb
-  lodsb					; MOV AL, [ESI]
+  mov	ESI, [EBP+24]       ; loop as many times as there are chars in what user entered
+  mov	ECX, [ESI]		    ;   (cont)
+  mov	EBX, 0			    ; tracks finalInteger
+  mov	ESI, [EBP+8]	    ; ESI = OFFSET userString
+  cld					    ; starting from the msb
+  lodsb					    ; MOV AL, [ESI]
 
   ; got the first ascii value in AL. 
   ; VALIDATE: check if the first char is a +/- sign
-  cmp	AL, 43			; is it a + sign?
+  cmp	AL, 43		    	; is it a + sign?
   jne	_isNotPlusSign
 
   ; it is a plus sign. Skip to next char, since + sign is redundant
   cld
   lodsb
-  dec	ECX				; start loop at next char, because first char's check is complete
+  dec	ECX		    		; start loop at next char, because first char's check is complete
   jmp	_buildInt		
 
   _isNotPlusSign:
-  mov   isNegative, 0
-  cmp	AL, 45			; is it a - sign?
+  push  EDI
+  mov	EDI, [EBP+32]       ; EDI = OFFSET isNegative
+  mov   [EDI], DWORD PTR 0	; isNegative = 0
+  pop	EDI  
+  cmp	AL, 45  			; is it a - sign?
 
   ; if not negative, leave isNegative clear and _buildInt
   jne	_buildInt
 
   ; else, it is negative. Set isNegative and _buildInt. Final step will be to convert from positive to negative by 0 - value
-  mov	isNegative, 1
+  push	EDI
+  mov	EDI, [EBP+32]	    ; EDI = OFFSET isNegative
+  mov	[EDI], DWORD PTR 1  ; isNegative = 1
+  pop	EDI
   cld
-  lodsb					; start loop at next char, beause first char's check is complete
+  lodsb					    ; start loop at next char, beause first char's check is complete
   dec	ECX
 
   ; convert the rest of the string to number form
@@ -251,18 +292,18 @@ ReadVal PROC
   ; first char already loaded into AL for first iteration
 
   ; VALIDATE: is it a number (between 48-57 in ASCII)?
-  cmp	AL, 48			; is it less than 48?
+  cmp	AL, 48			    ; is it less than 48?
   jl	_invalidInput
 
-  cmp	AL, 57			; is it more than 57?
+  cmp	AL, 57			    ; is it more than 57?
   jg	_invalidInput
 
-  jmp _validInput		; passed validation. Char is a number's ASCII code
+  jmp _validInput		    ; passed validation. Char is a number's ASCII code
 
   _invalidInput:
-  mov	EDX, [EBP+16]	; print invalid error message
+  mov	EDX, [EBP+16]	    ; print invalid error message
   call	WriteString
-  jmp	_getUserString  ; go all the way back to square 1 and _getUserString afresh
+  jmp	_getUserString      ; go all the way back to square 1 and _getUserString afresh
 
   _validInput:
   ; TODO: make this loop into a helper function convertAsciiToInt
@@ -273,46 +314,49 @@ ReadVal PROC
     ; EBX = finalInteger
   ; EBX = EBX * 10 + (AL - 48)
   
-  sub	AL, 48		    ; AL - 48 (aka, nextDigit to add)
-;  mov	temp, AL		; temp = AL		TODO: make local nextDigit
-  mov	DL, AL			;				TODO: delete after local nextDigit
+  sub	AL, 48		        ; AL - 48 (aka, nextDigit to add)
+;  mov	temp, AL		    ; temp = AL		TODO: make local nextDigit
+  mov	DL, AL			    ;				TODO: delete after local nextDigit
   
-  push	EAX				; save EAX
+  push	EAX				    ; save EAX
   mov	EAX, 10
-  push	EDX				; preserve temp EDX for mul
-  mul	EBX				; EAX = EBX * 10
-  mov	EBX, EAX		; EBX = EAX
-  pop	EDX				; restore EDX after mul
-  pop	EAX				; restore EAX
+  push	EDX				    ; preserve temp EDX for mul
+  mul	EBX				    ; EAX = EBX * 10
+  mov	EBX, EAX		    ; EBX = EAX
+  pop	EDX				    ; restore EDX after mul
+  pop	EAX				    ; restore EAX
 
 ;  add	EBX, DWORD PTR temp ; EBX = EBX + temp
   push	EBX
   movzx EBX, DL
-  mov   EDX, EBX		;				TODO: maybe delete after local nextDigit
+  mov   EDX, EBX		    ;				TODO: maybe delete after local nextDigit
   pop	EBX
-  add	EBX, EDX		;				TODO: delete after local nextDigit
+  add	EBX, EDX		    ;				TODO: delete after local nextDigit
 
   ; maintain loop
-  cld					; iterate left to right
-  lodsb				    ; MOV AL, [ESI] then inc ESI
+  cld					    ; iterate left to right
+  lodsb				        ; MOV AL, [ESI] then inc ESI
 
   loop _buildInt
 
   ; if isNegative, subtract value (EBX) from 0
-  cmp	isNegative, 1
-  jne	_saveAsUserInt	; nothing to do--not negative!
+  push	ESI
+  mov	ESI, [EBP+32]
+  cmp	[ESI], DWORD PTR 1  ; compare isNegative to 1 (true)
+  pop	ESI
+  jne	_saveAsUserInt	    ; nothing to do--not negative!
 
   ; negate value
   push	EAX
   mov	EAX, 0
-  sub	EAX, EBX		; EAX = 0 - EBX
-  mov	EBX, EAX		; EBX = EAX
+  sub	EAX, EBX		    ; EAX = 0 - EBX
+  mov	EBX, EAX		    ; EBX = EAX
   pop   EAX
 
   _saveAsUserInt:
   ; save finalInteger as userInt
-  mov	EDI, [EBP+28]   ; EDI = OFFSET userInt
-  mov	[EDI], EBX		; userInt = EBX
+  mov	EDI, [EBP+28]       ; EDI = OFFSET userInt
+  mov	[EDI], EBX		    ; userInt = EBX
 
   pop   EDI
   pop   ESI
